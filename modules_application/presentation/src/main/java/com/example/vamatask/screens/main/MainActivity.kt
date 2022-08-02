b22df4bea06cb590e3c81470b699797abcdae804
Karol Michalski <karol.michalski95@gmail.com>
2022-08-02 21:17:29 +0200

    Init

---
+++ modules_application/presentation/src/main/java/com/example/vamatask/screens/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vamatask.screens.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.vamatask.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
