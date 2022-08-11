0e16c211689c70c92afc5782e2ca5d0b52e8549a
Karol Michalski <karol.michalski95@gmail.com>
2022-08-11 09:59:31 +0200

    Remove unused classes
    (cherry picked from commit 6fe4de57555a1d645333d8c91b920538bff28f00)

--- modules_application/domain/src/main/java/com/example/domain/di/AlbumModule.kt
+++ modules_application/domain/src/main/java/com/example/domain/di/AlbumModule.kt
@@ -2,11 +2,8 @@ package com.example.domain.di
 
 import com.example.domain.AlbumRepository
 import com.example.domain.AlbumRepositoryImpl
-import com.example.domain.usecases.AlbumInteractor
-import com.example.domain.usecases.AlbumUseCases
 import org.koin.dsl.module
 
 val albumModule = module {
-    single<AlbumUseCases> { AlbumInteractor(get()) }
     single<AlbumRepository> { AlbumRepositoryImpl(get(), get()) }
 }
--- modules_application/domain/src/main/java/com/example/domain/usecases/AlbumUseCases.kt
+++
@@ -1,8 +0,0 @@
-package com.example.domain.usecases
-
-import com.example.data.models.Album
-import io.reactivex.rxjava3.core.Single
-
-interface AlbumUseCases {
-    fun getAlbums(amount: Int): Single<List<Album>>
-}
